cdf435e75b2402e62e18163cfb04f5efc11cc7b4
Lukasz <lstaszew@gmail.com>
2018-04-11 00:04:33 +0200

    Update NoiseCalculator.java
    Commit added on remote repository (github.com)

--- src/main/java/pl/codemakesfun/NoiseCalculator.java
+++ src/main/java/pl/codemakesfun/NoiseCalculator.java
@@ -1,9 +1,15 @@
 package pl.codemakesfun;
 
+/*
+ *	Comment added on github.com
+ */
 public class NoiseCalculator {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world..");
+		int[] testArray = {9, 3, 2, 1, 7, 4, 5};
+		for(Integer i:testArray) {
+			System.out.println("Element in array has following value: " + i);
+			}
 	}
 
 }
