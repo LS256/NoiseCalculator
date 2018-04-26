68b24714a431a384c38656afb2ca036f684b1cee
LS256 <lstaszew@gmail.com>
2018-04-26 23:53:52 +0200

    Added class for caculatig distance between two points - using pitagoras assumption

--- src/main/java/pl/codemakesfun/NoiseCalculator.java
+++ src/main/java/pl/codemakesfun/NoiseCalculator.java
@@ -6,11 +6,9 @@ package pl.codemakesfun;
 public class NoiseCalculator {
 
 	public static void main(String[] args) {
-		int[] testArray = {9, 3, 2, 1, 7, 4, 5};
-		for(Integer i:testArray) {
-			System.out.println("Element in array has following value: " + i);
-			}
-		System.out.println("Koniec");
+
+		
+		
 	}
 
 }
